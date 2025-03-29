4c0d468e5008c4def35d1eba8af722f9b91d9123
Stefan Munnes <munnes@wzb.eu>
2025-03-29 15:05:28 +0100

    clean var names and add package dependencies

--- scripts/load_data.R
+++ scripts/load_data.R
@@ -1,16 +1,15 @@
-library(readr)
+data_municipal <- readr::read_csv("data/municipal/municipal_main.csv")
 
-data_municipal <- read_csv("data/municipal/municipal_main.csv")
-
-data_pks_2020 <- read_delim(
+data_pks_2020 <- readr::read_delim(
   "data/pks/KR-F-01-T01-Kreise-Faelle-HZ_csv.csv",
   delim = ";",
   skip = 1,
   na = c("", "NA", "------"),
   locale = locale(encoding = "Latin1")
-)
+) |>
+  janitor::clean_names()
 
 
-data_immo <- read_csv("data/immoscout/panel/CampusFile_HK_cities.csv")
+data_immo <- readr::read_csv("data/immoscout/panel/CampusFile_HK_cities.csv")
 
-data_immo_2020 <- filter(data_immo, stringr::str_detect(adat, "2020"))
+data_immo_2020 <- dplyr::filter(data_immo, stringr::str_detect(adat, "2020"))
