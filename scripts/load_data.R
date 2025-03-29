45b43a7574be0158003e22bf9897356e9e30e928
Stefan Munnes <munnes@wzb.eu>
2025-03-29 11:56:24 +0100

    add immo data for 2020

--- scripts/load_data.R
+++ scripts/load_data.R
@@ -6,6 +6,11 @@ data_pks_2020 <- read_delim(
   "data/pks/KR-F-01-T01-Kreise-Faelle-HZ_csv.csv",
   delim = ";",
   skip = 1,
+  na = c("", "NA", "------"),
   locale = locale(encoding = "Latin1")
 )
 
+
+data_immo <- read_csv("data/immoscout/panel/CampusFile_HK_cities.csv")
+
+data_immo_2020 <- filter(data_immo, stringr::str_detect(adat, "2020"))
