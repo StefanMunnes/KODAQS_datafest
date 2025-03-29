23e894f802c381c14d87788771492fe5620c972c
Stefan Munnes <munnes@wzb.eu>
2025-03-29 10:07:59 +0100

    add encoding for umlaute

--- scripts/load_data.R
+++ scripts/load_data.R
@@ -1,9 +1,11 @@
 library(readr)
 
+data_municipal <- read_csv("data/municipal/municipal_main.csv")
 
-data_pks <- read_delim(
+data_pks_2020 <- read_delim(
   "data/pks/KR-F-01-T01-Kreise-Faelle-HZ_csv.csv",
   delim = ";",
-  skip = 1
+  skip = 1,
+  locale = locale(encoding = "Latin1")
 )
 
