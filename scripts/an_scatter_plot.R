9aec0a8001f55136aed7160c3720d8288643145a
Stefan Munnes <munnes@wzb.eu>
2025-03-30 10:40:26 +0200

    create scatter plot of crime rate and flat size by cluster

---
+++ scripts/an_scatter_plot.R
@@ -0,0 +1,37 @@
+data_analysis <- read.csv("data/data_kreis_pks_2022_cluster.csv") |>
+  select(haeufigkeitszahl, all_of(varlist_analysis), cluster) |>
+  mutate(
+    cluster = factor(
+      as.character(cluster),
+      levels = c("2", "3", "4", "1"),
+      labels = lab_cluster
+    )
+  )
+
+
+plot_scatter <- ggplot(data = data_analysis) +
+  geom_point(
+    aes(
+      x = mean_FLAT_size_2022,
+      y = haeufigkeitszahl,
+      color = cluster
+    ),
+    size = 3,
+    alpha = 0.8
+  ) +
+  scale_color_manual(values = clr_cluster) +
+  labs(
+    y = "Crime rate / 100'000 people",
+    x = "Flat size (m2)"
+  ) +
+  theme_minimal() +
+  theme(legend.position = "none")
+
+
+ggsave(
+  "output/plot_scatter.png",
+  plot_scatter,
+  width = 7,
+  height = 7,
+  dpi = 300
+)
