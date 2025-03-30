4ad4fe88061eb5baeca241c05a7a5dc510727b76
rajo1772 <leena.maass@uni-oldenburg.de>
2025-03-30 09:26:51 +0200

    # update

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -1,127 +1,144 @@
+##### packages -----------------------------------------------------------------
 library(dplyr)
 library(stringr)
+library(broom)
+library(ggplot2)
+library(ggfortify)
 
+##### options
 options(scipen = 100) # get rid of exponential notation (problem as character)
 
-
-# aggregate municipal data on "kreis" level (create mean/sum values forgemeinden)
-
+##### load data  ---------------------------------------------------------------
 data_kreis_pks_2022 <- read.csv("data/data_kreis_pks_2022.csv")
 data_kreis_pks_2022_cluster <- read.csv("data/data_kreis_pks_2022_cluster.csv")
 
 
-
-
 ##### Regression ANALYSIS ------------------------------------------------------
-library(margins)
 m0 <- lm(haeufigkeitszahl ~ 1, data = data_kreis_pks_2022)
 summary(m0) 
 
 #mit kreisart
-m1 <- lm(haeufigkeitszahl ~ pop20_MEAN + mean_FLAT_size_2022 + 
+m1 <- lm(haeufigkeitszahl ~
+         pop20_MEAN + 
+         mean_FLAT_size_2022 + 
          vac_MEAN_muni_2022 + st_einnkr + 
-         Rent_m2_EUR_2022 + POP_60_plus_._2022 + kreisart , data = data_kreis_pks_2022)
+         Rent_m2_EUR_2022 + 
+         POP_60_plus_._2022 + 
+         kreisart , data = data_kreis_pks_2022)
 summary(m1)     
 
-ame_m1 <- summary(margins(m1))
-
-
 #ohne kreisart
-library(ggplot2)
-library(ggfortify)
 m2 <- lm(haeufigkeitszahl ~ pop20_MEAN + mean_FLAT_size_2022 + #change_pc_bs_mean +
          vac_MEAN_muni_2022 + st_einnkr + 
          Rent_m2_EUR_2022 + POP_60_plus_._2022, data = data_kreis_pks_2022)
-summary(m2)     
-ame_m2 <- summary(margins(m2))
+summary(m2)
 
-
-
-ggplot(ame_m2, aes(x=AME, y= factor,xmin= lower, xmax= upper))+ 
-  geom_point(position= position_dodge(0.4), size=10)+
-  geom_errorbarh(position= position_dodge(0.4), height= 0.2)+
-  scale_y_discrete(limits= rev(levels(ame_m2$factor)))+
-  labs(y= lab_vars,x= "AMEs (DV: Skip at least 1 scenario-based question)")+
-  theme_bw()+
-  theme(legend.title = element_blank(),
-        legend.position= "none",
-        axis.title.x = element_text(size=15),
-        axis.text.y = element_text(size = 30),
-        axis.text.x= element_text(size=30))
-
-
-
-write.csv(ame_m2, "ame_m2.csv")
-
-#
+# regression assumptions
 autoplot(m2, label.size=3)
 
+# Extract & visualise coefficients of model m2
+coef_m2 <- tidy(m2) %>%
+  filter(term != "(Intercept)") %>%  # Intercept ausblenden
+  ggplot(aes(x = estimate, y = reorder(term, estimate))) +
+  geom_point(size = 3, color = "0070C0", shape= 16) +  
+  geom_errorbarh(aes(xmin = estimate - 1.96 * std.error, 
+                     xmax = estimate + 1.96 * std.error), 
+                 height = 0.2, color = "black") +
+  geom_vline(xintercept= 0, linetype = "dotted", color = "dark grey")+
+  scale_y_discrete(labels = c("mean_FLAT_size_2022" = "flatsize 22 (mean)",
+                              "POP_60_plus_._2022"= "Old population",
+                              "pop20_MEAN" = "pop_20 (mean)",
+                              "Rent_m2_EUR_2022" = "Rent/m2 22 (€)",
+                              "st_einnkr" = "tax/capita 22 (€)",
+                              "vac_MEAN_muni_2022" = "vacancy rate/municipality (mean)"))+
+  labs(title = "Coefficient Plot", x = "estimates (95% CI)", y = "predictors") +
+  theme_minimal()
+
+ggsave("graph/coef_m2.png", width = 8, height = 6, dpi = 300)
 
 
 
-# Koeffizienten extrahieren & visualisieren
-library(dotwhisker)
-
-plot(margins(ame_m2))
 
 
-coef <- dwplot(ame_m2,
-               vline = geom_vline(
-                 xintercept = 0,
-                 colour = "grey60",
-                 linetype = 2
-               )) +
-  theme_minimal() +
-  theme(plot.title = element_text(face = "bold"),
-        legend.position = c(0.007, 0.01),
-        variable.names=lab_vars) + 
-  ggtitle("Coefficientplot Frequency Figure for Violent Crime") +
-  theme(legend.position = "none") 
-ggsave("graph/coef.png", width = 8, height = 6, dpi = 300)
-
-
-### Regression including clusters 
+##### Regression including clusters  -------------------------------------------
 table(data_kreis_pks_2022_cluster$cluster)
 data_kreis_pks_2022_cluster$cluster <- as.character(data_kreis_pks_2022_cluster$cluster)
 
 m3_0 <- lm(haeufigkeitszahl ~ cluster , data = data_kreis_pks_2022_cluster)
 summary(m3_0)   
 
-m3_1 <- lm(haeufigkeitszahl ~ pop20_MEAN + mean_FLAT_size_2022 + #change_pc_bs_mean +
-           vac_MEAN_muni_2022 + st_einnkr + 
-           Rent_m2_EUR_2022 + POP_60_plus_._2022 + cluster , data = data_kreis_pks_2022_cluster)
-
-m3_2 <- lm(haeufigkeitszahl ~ pop20_MEAN + mean_FLAT_size_2022 + #change_pc_bs_mean +
+m3_1 <- lm(haeufigkeitszahl ~ pop20_MEAN + mean_FLAT_size_2022 +
              vac_MEAN_muni_2022 + st_einnkr + 
              Rent_m2_EUR_2022 + POP_60_plus_._2022 , data = data_kreis_pks_2022_cluster)
 
-screenreg(list(m3_0,m3_2,m3_1), digits=4, custom.model.names = c("Only Cluster","Full model without clusters", "full model with cluster"), variable.names = lab_vars) 
+m3_2 <- lm(haeufigkeitszahl ~ pop20_MEAN + mean_FLAT_size_2022 +
+           vac_MEAN_muni_2022 + st_einnkr + 
+           Rent_m2_EUR_2022 + POP_60_plus_._2022 + cluster , data = data_kreis_pks_2022_cluster)
 
 
-m1_df <-
-  broom::tidy(m3_0) |> filter(term != "(Intercept)") |> mutate(model = "Model 1")
-m2_df <-
-  broom::tidy(m3_2) |> filter(term != "(Intercept)") |> mutate(model = "Model 2")
-m3_df <-
-  broom::tidy(m3_1) |> filter(term != "(Intercept)") |> mutate(model = "Model 3")
-three_models <- rbind(m1_df, m2_df,m3_df)
-
-dwplot(three_models)
+library(texreg)
+screenreg(list(m3_0,m3_1,m3_2), 
+          digits=4, 
+          custom.model.names = c("Only Cluster","Full model without clusters", "full model with cluster"), 
+          var.names = lab_vars) 
 
 
-dwplot(m2,
-               vline = geom_vline(
-                 xintercept = 0,
-                 colour = "grey60",
-                 linetype = 2
-               )) +
-  theme_minimal() +
-  theme(plot.title = element_text(face = "bold"),
-        legend.position = c(0.007, 0.01),
-        names = lab_vars) + 
-  ggtitle("Coefficientplot Frequency Figure for Violent Crime")
-ggsave("graph/coef.png", width = 8, height = 6, dpi = 300)
+#
+model_data <- bind_rows(
+  tidy(m3_0) %>% mutate(Model = "m3_0"),
+  tidy(m3_1) %>% mutate(Model = "m3_1"),
+  tidy(m3_2) %>% mutate(Model = "m3_2")) %>%
+  filter(term != "(Intercept)")  # Intercept ausblenden
+
+
+# m1_df <-
+#   broom::tidy(m3_0) |> filter(term != "(Intercept)") |> mutate(model = "Model 1")
+# m2_df <-
+#   broom::tidy(m3_1) |> filter(term != "(Intercept)") |> mutate(model = "Model 2")
+# m3_df <-
+#   broom::tidy(m3_2) |> filter(term != "(Intercept)") |> mutate(model = "Model 3")
+# three_models <- rbind(m1_df, m2_df,m3_df)
+# 
+# dwplot(three_models)
+# 
+# tidy(m2) %>%
+#   filter(term != "(Intercept)") %>%  # Intercept ausblenden
+ggplot(model_data, aes(x = estimate, y = reorder(term, estimate), color = Model)) +
+  geom_point(size = 3, color = "0070C0", shape= 16, position = position_dodge(width = 0.5)) +
+  geom_errorbarh(aes(xmin = estimate - 1.96 * std.error, 
+                     xmax = estimate + 1.96 * std.error), 
+                 height = 0.2, position = position_dodge(width = 0.5)) +
+  geom_vline(xintercept= 0, linetype = "dotted", color = "dark grey")+
+  scale_y_discrete(labels = c("mean_FLAT_size_2022" = "flatsize 22 (mean)",
+                              "POP_60_plus_._2022"= "Old population",
+                              "pop20_MEAN" = "pop_20 (mean)",
+                              "Rent_m2_EUR_2022" = "Rent/m2 22 (€)",
+                              "st_einnkr" = "tax/capita 22 (€)",
+                              "vac_MEAN_muni_2022" = "vacancy rate/municipality (mean)"))+
+  labs(title = "Coefficient Plot", x = "estimates (95% CI)", y = "predictors") +
+  theme_minimal()
 
 
 
 
+#Regressionsoutput Word
+model <- list(m3_0,m3_1,m3_2)
+
+
+# Word-Datei speichern
+library(pandoc)
+library(modelsummary)
+
+lab_vars <- c(
+  "(Intercept)" = "Intercept",
+  "pop20_MEAN" = "Mean population ",
+  "mean_FLAT_size_2022" = "Flat size in m2",
+  "vac_MEAN_muni_2022" = "Vacancy rate per district ",
+  "Rent_m2_EUR_2022" = "Rent per m² in € ",
+  "POP_60_plus_._2022" = "Population age 60+ ",
+  "st_einnkr" = "Tax revenue per capita in € ",
+  "cluster" = "Cluster"
+)
+
+modelsummary(model, coef_map = lab_vars,
+             output = "tables/regressionstabelle.docx")
