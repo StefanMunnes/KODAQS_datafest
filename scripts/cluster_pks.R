c7abeaa235cdea2b9fa89d99bee6bc735295b350
Stefan Munnes <munnes@wzb.eu>
2025-03-30 08:37:55 +0200

    add map for clusters

--- scripts/cluster_pks.R
+++ scripts/cluster_pks.R
@@ -1,24 +1,26 @@
 library(dplyr)
-library(corrplot)
+library(ggplot2)
 library(cluster)
 library(factoextra)
-library(tmap)
+library(gridExtra)
+library(cowplot)
 library(GGally)
+library(sf)
 
 set.seed(161) # For reproducibility
 
+data_kreis_pks_2022 <- read.csv("data/data_kreis_pks_2022.csv")
 
 data_analysis <- data_kreis_pks_2022 |>
   select(
-    # haeufigkeitszahl,
-    pop20_MEAN,
-    mean_FLAT_size_2022,
+    change_pc_bs_mean,
     vac_MEAN_muni_2022,
     Rent_m2_EUR_2022,
     share_larger_HH_2022,
-    POP_60_plus_._2022,
+    mean_FLAT_size_2022,
     st_einnkr,
-    change_pc_bs_mean
+    POP_60_plus_._2022,
+    pop20_MEAN
   )
 
 
@@ -71,17 +73,60 @@ data_analysis$cluster <- as.factor(km_res$cluster)
 
 data_analysis |>
   group_by(cluster) |>
-  summarise(across(everything(), mean, na.rm = TRUE)) |>
-  print()
+  summarise(across(everything(), mean, na.rm = TRUE))
 
-colors <- RColorBrewer::brewer.pal(k, "Set1")
+cluster_means <- data_analysis |>
+  mutate_at(vars(-cluster), ~ scale(.)[, 1]) |>
+  group_by(cluster) |>
+  summarize_all(mean) |>
+  tidyr::pivot_longer(-cluster, names_to = "variable", values_to = "value")
 
-GGally::ggparcoord(
-  data = data_analysis,
-  columns = 1:(ncol(data_analysis) - 1),
-  groupColumn = "cluster",
-  scale = "std",
-  alphaLines = 0.5
-) +
-  scale_color_manual(values = colors) +
+
+# ---- 1. Cluster by variables ----
+data_analysis |>
+  mutate(
+    change_pc_bs_mean = ifelse(change_pc_bs_mean >= 25, 25, change_pc_bs_mean)
+  ) |>
+  ggparcoord(
+    columns = 1:(ncol(data_analysis) - 1),
+    groupColumn = "cluster",
+    scale = "std",
+    alphaLines = 0.15
+  ) +
+  ungeviz::geom_hpline(
+    data = cluster_means,
+    aes(x = variable, y = value, colour = cluster),
+    alpha = 0.8,
+    size = 3,
+    inherit.aes = FALSE
+  ) +
+  geom_hline(yintercept = 0, color = "grey20") +
+  coord_flip() +
+  scale_x_discrete(labels = lab_vars) +
+  scale_color_manual(values = clr_cluster) +
+  labs(
+    y = "Scaled values",
+    x = "",
+    title = "Cluster by variables",
+    color = "Cluster"
+  ) +
   theme_minimal()
+
+
+# ---- 1. Map ----
+
+# prepare the shapefile
+data_map <- data_shapefile |>
+  filter(AGS != "09672") |> # remove Bad Kissing because of missing data
+  mutate(AGS = as.numeric(AGS), cluster = km_res$cluster)
+
+
+# Plot the map
+ggplot(data_map) +
+  geom_sf(aes(fill = as.factor(cluster)), color = "#747474", size = 0.02) +
+  scale_fill_manual(values = clr_cluster, na.value = "grey50") +
+  theme_minimal() +
+  labs(
+    title = "Cluster for Municipalities in Germany",
+    fill = "Cluster"
+  )
