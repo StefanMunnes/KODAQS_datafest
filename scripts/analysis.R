085930e47eced59612912e930d11e65703ef4956
rajo1772 <leena.maass@uni-oldenburg.de>
2025-03-30 11:10:52 +0200

    # update

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -17,41 +17,9 @@ data_kreis_pks_2022 <- read.csv("data/data_kreis_pks_2022.csv")
 data_kreis_pks_2022_cluster <- read.csv("data/data_kreis_pks_2022_cluster.csv")
 
 
-##### Regression analysis without clusters -------------------------------------
-m0 <- lm(haeufigkeitszahl ~ 1, data = data_kreis_pks_2022)
-summary(m0) 
 
-m1 <- lm(haeufigkeitszahl ~ 
-         pop20_MEAN + 
-         mean_FLAT_size_2022 +
-         vac_MEAN_muni_2022 + 
-         st_einnkr + 
-         Rent_m2_EUR_2022 + 
-         POP_60_plus_._2022, 
-         data = data_kreis_pks_2022)
-summary(m1)
 
-# regression assumptions
-autoplot(m1, label.size=3)
 
-# Extract & visualise coefficients of model m2
-coef_m1 <- tidy(m1) %>%
-  filter(term != "(Intercept)") %>%  # Intercept ausblenden
-  ggplot(aes(x = estimate, y = reorder(term, estimate))) +
-  geom_point(size = 3, color = "0070C0", shape= 16) +  
-  geom_errorbarh(aes(xmin = estimate - 1.96 * std.error, 
-                     xmax = estimate + 1.96 * std.error), 
-                 height = 0.2, color = "black") +
-  geom_vline(xintercept= 0, linetype = "dotted", color = "dark grey")+
-  scale_y_discrete(labels = c("mean_FLAT_size_2022" = "flatsize 22 (mean)",
-                              "POP_60_plus_._2022"= "Old population",
-                              "pop20_MEAN" = "pop_20 (mean)",
-                              "Rent_m2_EUR_2022" = "Rent/m2 22 (€)",
-                              "st_einnkr" = "tax/capita 22 (€)",
-                              "vac_MEAN_muni_2022" = "vacancy rate/municipality (mean)"))+
-  labs(title = "Coefficient Plot", x = "estimates (95% CI)", y = "predictors") +
-  theme_minimal()
-ggsave("graph/coef_m1.png", width = 8, height = 6, dpi = 300)
 
 
 
@@ -77,14 +45,38 @@ m2_1 <- lm(haeufigkeitszahl ~
            change_pc_bs_mean, 
            data = data_kreis_pks_2022_cluster)
 
+# regression assumptions
+autoplot(m2_1, label.size=3)
+
+
+# # Extract & visualise coefficients of model m2
+# coef_m1 <- tidy(m2_1) %>%
+#   filter(term != "(Intercept)") %>%  # Intercept ausblenden
+#   ggplot(aes(x = estimate, y = reorder(term, estimate))) +
+#   geom_point(size = 3, color = "0070C0", shape= 16) +  
+#   geom_errorbarh(aes(xmin = estimate - 1.96 * std.error, 
+#                      xmax = estimate + 1.96 * std.error), 
+#                  height = 0.2, color = "black") +
+#   geom_vline(xintercept= 0, linetype = "dotted", color = "dark grey")+
+#   scale_y_discrete(labels = c("mean_FLAT_size_2022" = "flatsize 22 (mean)",
+#                               "POP_60_plus_._2022"= "Old population",
+#                               "pop20_MEAN" = "pop_20 (mean)",
+#                               "Rent_m2_EUR_2022" = "Rent/m2 22 (€)",
+#                               "st_einnkr" = "tax/capita 22 (€)",
+#                               "vac_MEAN_muni_2022" = "vacancy rate/municipality (mean)"))+
+#   labs(title = "Coefficient Plot", x = "estimates (95% CI)", y = "predictors") +
+#   theme_minimal()
+# ggsave("graph/coef_m1.png", width = 8, height = 6, dpi = 300)
+
+
 modelsummary(list(m2_0,m2_1), 
           digits=4) 
 
 
-#
+# Plot
 model_data <- bind_rows(
-  tidy(m2_0) %>% mutate(Model = "model1"),
-  tidy(m2_1) %>% mutate(Model = "model2")) %>%
+  tidy(m2_0) %>% mutate(Model = "Model 1: Only Clusters (R²: 45 %)"),
+  tidy(m2_1) %>% mutate(Model = "Model 2: Full (R²: 63.6 %)")) %>%
   filter(term != "(Intercept)")  # Intercept ausblenden
 
 coef_m2 <- ggplot(model_data, aes(x = estimate, y = reorder(term, estimate), color = Model)) +
@@ -93,6 +85,8 @@ coef_m2 <- ggplot(model_data, aes(x = estimate, y = reorder(term, estimate), col
                      xmax = estimate + 1.96 * std.error), 
                  height = 0.2, position = position_dodge(width = 0.5)) +
   geom_vline(xintercept= 0, linetype = "dotted", color = "dark grey")+
+  theme_minimal() + 
+  theme(legend.position = "bottom") +
   scale_y_discrete(labels = c("mean_FLAT_size_2022" = "flatsize 22 (mean)",
                               "change_pc_bs_mean" = "Built-up area change in % ('05–'20)",
                               "POP_60_plus_._2022"= "Old population",
@@ -104,8 +98,7 @@ coef_m2 <- ggplot(model_data, aes(x = estimate, y = reorder(term, estimate), col
                               "cluster2" = "Cluster 2",
                               "cluster3" = "Cluster 3",
                               "cluster4" = "Cluster 4"))+
-  labs(title = "Coefficient Plot Regression Analysis", x = "estimates (95% CI)", y = "predictors") +
-  theme_minimal()
+  labs(x = "estimates (95% CI)", y = "predictors")
 ggsave("graph/coef_m2.png", width = 8, height = 6, dpi = 300)
 
 
