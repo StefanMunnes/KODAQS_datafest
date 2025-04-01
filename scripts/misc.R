68a750dd8641776e34a03527b11e630bacef8cda
Stefan Munnes <munnes@wzb.eu>
2025-04-01 11:22:30 +0200

    better variable names; clean old colors; add names for factor color labels with code

--- scripts/misc.R
+++ scripts/misc.R
@@ -21,14 +21,14 @@ varlist_analysis <- c(
 
 # add labels vor variables and clusters
 lab_vars <- c(
-  "Haeufigkeitszahl" = "Crime rate per capita",
-  "pop20_MEAN" = "Mean population ",
-  "mean_FLAT_size_2022" = "Flat size in m2",
-  "vac_MEAN_muni_2022" = "Vacancy rate per district ",
-  "Rent_m2_EUR_2022" = "Rent per m² in € ",
-  "share_larger_HH_2022" = "Share of large households ",
-  "POP_60_plus_._2022" = "Population age 60+ ",
-  "st_einnkr" = "Tax revenue per capita in € ",
+  "Haeufigkeitszahl" = "Crime rate per 10'000 capita",
+  "pop20_MEAN" = "Population density",
+  "mean_FLAT_size_2022" = "Flat size in m²",
+  "vac_MEAN_muni_2022" = "Vacancy rate",
+  "Rent_m2_EUR_2022" = "Rent per m² in €",
+  "share_larger_HH_2022" = "Share of large households",
+  "POP_60_plus_._2022" = "Share of population age 60+",
+  "st_einnkr" = "Tax revenue per capita in €",
   "change_pc_bs_mean" = "Built-up area change in % ('05–'20)"
 )
 
@@ -41,30 +41,11 @@ lab_cluster <- c(
 
 
 # define colors for cluster visualization
-# clr_cluster <- c(
-#   "#A9A9A9",
-#   "#1E90FF",
-#   "#32CD32",
-#   "#FF8C00"
-# )
-
-# clr_cluster <- c(
-#   "#696969",
-#   "#4682B4",
-#   "#228B22",
-#   "#B22222"
-# )
-
 clr_cluster <- c(
-  "Poor, aging, vacant, low-density areas" = "#FBCA7B",
-  "Dense, aging, costly, shrinking housing" = "#A1C6EA",
-  "Growing, spacious, rural family areas" = "#B4E7B0",
-  "Wealthy, young, dense urban cores" = "#F4A7B9"
+  "#FBCA7B",
+  "#A1C6EA",
+  "#B4E7B0",
+  "#F4A7B9"
 )
 
-# clr_cluster <- c(
-#   "#FBCA7B",
-#   "#F4A7B9",
-#   "#A1C6EA",
-#   "#B4E7B0"
-# )
+names(clr_cluster) <- lab_cluster
